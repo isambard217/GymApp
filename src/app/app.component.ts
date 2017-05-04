e917504aa9e927e2e07463b1f7aa16ea1003092b
Isambard Wheeler-Artwell <wheelerisambard@hotmail.co.uk>
2017-05-04 18:00:05 +0100

    working on services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
+import { CourseComponentComponent } from './component/course-component/course-component.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  directives: [CourseComponentComponent],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { FacebookloginComponent } from './component/facebooklogin/facebooklogin.component';
+import { FacebookLoginComponent } from './component/facebooklogin/facebooklogin.component';
+import { CourseComponentComponent } from './component/course-component/course-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FacebookloginComponent
+    FacebookLoginComponent,
+    CourseComponentComponent
   ],
   imports: [
     BrowserModule,
